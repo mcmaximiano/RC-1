7a08cfd1262a230152fbbcaecf5aa40359dea1ec
Sérgio <sergio.s.carvalho@tecnico.ulisboa.pt>
2016-09-29 08:57:21 +0100

    Create server.py

---
+++ server.py
@@ -0,0 +1,16 @@
+PORT = 58000
+
+from socket import\
+        socket,\
+        AF_INET,\
+        SOCK_STREAM,\
+        gethostname
+
+welcome = socket(AF_INET, SOCK_STREAM)
+welcome.bind((gethostname(), PORT))
+welcome.listen(10)
+(serving_socket, client_addr) = welcome.accept()
+msg = serving_socket.recv(1000)
+welcome.close()
+serving_socket.close()
+print(msg.decode())
